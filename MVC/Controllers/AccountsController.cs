150469af33f8d17bac83acc34161f2f1f13092fe
omid salangi <omidsalangi@outlook.com>
2021-09-13 22:19:37 +0430

    connect to google complete

--- MVC/Controllers/AccountsController.cs
+++ MVC/Controllers/AccountsController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Application.Interface;
 using Application.ViewModel;
@@ -105,6 +106,9 @@ namespace MVC.Controllers
                 return View(model);
             }
 
+            model.ReturnUrl = returnUrl; // important
+            model.ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+
           var res= await _signInManager.PasswordSignInAsync(model.username, model.password, model.rememberme, true);
 
           if (res.Succeeded)
@@ -157,6 +161,75 @@ namespace MVC.Controllers
             var result = await _userManager.ConfirmEmailAsync(user, token);
 
             return Content(result.Succeeded ? "Email Confirmed" : "Email Not Confirmed");
+        }
+        [HttpPost]
+        public async Task<IActionResult> ExternalLogin(string provider, string returnUrl) // if name of form and input of one input ofthis action are same value of submit button will be pass to that input   this will be happen in post and from a form
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallBack", "Accounts",
+                new { ReturnUrl = returnUrl });
+
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);// connect to google
+            return new ChallengeResult(provider, properties);
+        }
+        public async Task<IActionResult> ExternalLoginCallBack(string returnUrl = null, string remoteError = null) // google response will call this action
+        {
+            returnUrl =
+                (returnUrl != null && Url.IsLocalUrl(returnUrl)) ? returnUrl : Url.Content("~/");
+
+            var loginViewModel = new LoginModel()
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+
+            if (remoteError != null)
+            {
+                ModelState.AddModelError("", $"Error : {remoteError}");
+                return View("Login", loginViewModel);
+            }
+
+            var externalLoginInfo = await _signInManager.GetExternalLoginInfoAsync(); // get data from google
+            if (externalLoginInfo == null)
+            {
+                ModelState.AddModelError("ErrorLoadingExternalLoginInfo", $"مشکلی پیش آمد");
+                return View("Login", loginViewModel);
+            }
+
+            var signInResult = await _signInManager.ExternalLoginSignInAsync(externalLoginInfo.LoginProvider,
+                externalLoginInfo.ProviderKey, false, true);
+
+            if (signInResult.Succeeded)
+            {
+                return Redirect(returnUrl);
+            }
+
+            var email = externalLoginInfo.Principal.FindFirstValue(ClaimTypes.Email); // if user is new  this code will be runed.
+
+            if (email != null)
+            {
+                var user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    var userName = email;//.Split('@')[0]; if we want use before @ in email address instead of username
+                    user = new IdentityUserChange()
+                    {
+                        UserName = (userName.Length <= 10 ? userName : userName.Substring(0, 10)),
+                        Email = email,
+                        EmailConfirmed = true
+                    };
+
+                    await _userManager.CreateAsync(user);
+                }
+
+                await _userManager.AddLoginAsync(user, externalLoginInfo);
+                await _signInManager.SignInAsync(user, false);
+
+                return Redirect(returnUrl);
+            }
+
+            ViewBag.ErrorTitle = "لطفا با بخش پشتیبانی تماس بگیرید";
+            ViewBag.ErrorMessage = $"دریافت کرد {externalLoginInfo.LoginProvider} نمیتوان اطلاعاتی از";
+            return View();
         }
     }
 }
